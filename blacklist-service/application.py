bf0e4b0f6cc2fe445ff81da56366f3aa2bf62ced
Jedom11 <j.dominguezp@uniandes.edu.co>
2026-05-24 16:52:54 -0500

    fix: apikey new relic updated

--- blacklist-service/application.py
+++ blacklist-service/application.py
@@ -1,7 +1,9 @@
+import newrelic.agent
+newrelic.agent.initialize('newrelic.ini')
+
 from app import create_app
 
-application = create_app()
-
+application = newrelic.agent.WSGIApplicationWrapper(create_app())
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=8080)
